7b5908791094d57b8f862cb8a4c074deb3d593a6
Guilherme Yazbek <guicansado@gmail.com>
2018-10-10 10:24:59 -0700

    Added unit test project to solution.

--- Unit Testing/Program.cs
+++ Unit Testing/Program.cs
@@ -2,9 +2,9 @@
 
 namespace Unit_Testing
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
---
+++ UnitTestingTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static Unit_Testing.Program;
+
+namespace UnitTestingTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
