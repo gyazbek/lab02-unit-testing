714772aba7663bb4032931b02c53a9bdbfe0a24e
Guilherme Yazbek <guicansado@gmail.com>
2018-10-10 10:55:22 -0700

    Added intro write line for the interface.

--- Unit Testing/Program.cs
+++ Unit Testing/Program.cs
@@ -14,6 +14,14 @@ namespace Unit_Testing
         /// </summary>  
         public static void UI()
         {
+            Console.WriteLine("       ,'``.._   ,'``.\r\n      :,--._:)\\,:,._,.:       HYPNO TOAD\r\n      :`--,''   :`...';\\      FUTURE BANK!\r\n       `,'       `---'  `.\r\n       /                 :\r\n      /                   \\\r\n    ,'                     :\\.___,-.\r\n   `...,---'``````-..._    |:       \\\r\n     (                 )   ;:    )   \\  _,-.\r\n      `.              (   //          `'    \\\r\n       :               `.//  )      )     , ;\r\n     ,-|`.            _,'/       )    ) ,' ,'\r\n    (  :`.`-..____..=:.-':     .     _,' ,'\r\n     `,'\\ ``--....-)='    `._,  \\  ,') _ '``._\r\n  _.-/ _ `.       (_)      /     )' ; / \\ \\`-.'\r\n `--(   `-:`.     `' ___..'  _,-'   |/   `.)\r\n     `-. `.`.``-----``--,  .'\r\n       |/`.\\`'        ,',');\r\n           `         (/  (/");
+            Console.WriteLine("Please make a selection: ");
+
+            bool prompt = true;
+            while (prompt)
+            {
+
+            }
 
         }
     }
