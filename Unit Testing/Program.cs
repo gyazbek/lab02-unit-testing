a7d8560bf8e93209ca2071ab6758386bab0b3ee4
Guilherme Yazbek <guicansado@gmail.com>
2018-10-10 10:59:07 -0700

    Added UI Choices convenience method.

--- Unit Testing/Program.cs
+++ Unit Testing/Program.cs
@@ -16,13 +16,26 @@ namespace Unit_Testing
         {
             Console.WriteLine("       ,'``.._   ,'``.\r\n      :,--._:)\\,:,._,.:       HYPNO TOAD\r\n      :`--,''   :`...';\\      FUTURE BANK!\r\n       `,'       `---'  `.\r\n       /                 :\r\n      /                   \\\r\n    ,'                     :\\.___,-.\r\n   `...,---'``````-..._    |:       \\\r\n     (                 )   ;:    )   \\  _,-.\r\n      `.              (   //          `'    \\\r\n       :               `.//  )      )     , ;\r\n     ,-|`.            _,'/       )    ) ,' ,'\r\n    (  :`.`-..____..=:.-':     .     _,' ,'\r\n     `,'\\ ``--....-)='    `._,  \\  ,') _ '``._\r\n  _.-/ _ `.       (_)      /     )' ; / \\ \\`-.'\r\n `--(   `-:`.     `' ___..'  _,-'   |/   `.)\r\n     `-. `.`.``-----``--,  .'\r\n       |/`.\\`'        ,',');\r\n           `         (/  (/");
             Console.WriteLine("Please make a selection: ");
-
+            
             bool prompt = true;
             while (prompt)
             {
+                Console.WriteLine(UIChoices());
+                Console.ReadLine();
 
             }
 
+        }
+
+        /// <summary>  
+        ///  Convenience method containing our ATM Choices for our UI prompt.
+        /// </summary>  
+        public static string UIChoices()
+        {
+            return "1) View Balance\n" +
+                "2) Withdraw Money\n" +
+                "3) Add Money\n" +
+                "4) Exit\n";
         }
     }
 }
