cf85d1d66ebf261ddea2c0f4569bdd0ecd6e6b1e
Guilherme Yazbek <guicansado@gmail.com>
2018-10-10 12:19:05 -0700

    Added tests for every method for every non void methods.

--- UnitTestingTest/UnitTest1.cs
+++ UnitTestingTest/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace UnitTestingTest
         [Fact]
         public void CanDepositPositive()
         {
-            Assert.Equal(true, DepositMoney(1000m));
+            Assert.True(DepositMoney(1000m));
         }
 
         [Fact]
@@ -18,20 +18,21 @@ namespace UnitTestingTest
         {
             balance = 0m;
             DepositMoney(500m);
-            Assert.Equal(balance,500);
+            Assert.Equal(500, balance);
         }
 
 
         [Fact]
         public void CantDepositNegative()
         {
-            Assert.Equal(false, DepositMoney(-1000m));
+            Assert.False(DepositMoney(-1000m));
         }
 
         [Fact]
         public void CanWithdrawPositive()
         {
-            Assert.Equal(true, WithdrawMoney(1000m));
+            balance = 1000;
+            Assert.True(WithdrawMoney(1000m));
         }
 
         [Fact]
@@ -39,14 +40,91 @@ namespace UnitTestingTest
         {
             balance = 500m;
             WithdrawMoney(500m);
-            Assert.Equal(balance, 0);
+            Assert.Equal(0, balance);
         }
 
 
         [Fact]
         public void CantWithdrawNegative()
         {
-            Assert.Equal(false, WithdrawMoney(-1000m));
+            Assert.False(WithdrawMoney(-1000m));
+        }
+
+        [Fact]
+        public void CanProcessMoneyAsString()
+        {
+            Assert.Equal(100m, ProcessMoney("100"));
+        }
+
+        [Fact]
+        public void CanProcessChoiceAsString()
+        {
+            Assert.Equal(2, ProcessChoice("2"));
+        }
+
+        
+
+        [InlineData(200)]
+        [InlineData(101)]
+        [Theory]
+        public void CantWithdrawWithInsufficientBalance(decimal money)
+        {
+            balance = 100;
+            WithdrawMoney(money);
+            Assert.Equal(100, balance);
+        }
+
+        [InlineData("2")]
+        [InlineData("1")]
+        [Theory]
+        public void ProcessChoiceNoExceptionThrown(string choice)
+        {
+            ProcessChoice(choice);
+            Assert.True(true);
+        }
+
+        [InlineData("2z")]
+        [InlineData("OneHundred")]
+        [InlineData("5")]
+        [InlineData("0")]
+        [Theory]
+        public void ProcessChoiceExceptionThrown(string choice)
+        {
+            try
+            {
+                ProcessChoice(choice);
+            }
+            catch
+            {
+                Assert.True(true);
+            }
+
+        }
+
+
+        [InlineData("2")]
+        [InlineData("1000.0")]
+        [Theory]
+        public void ParseMoneyNoExceptionThrown(string money)
+        {
+            ProcessMoney(money);
+            Assert.True(true);
+        }
+
+        [InlineData("$100")]
+        [InlineData("-100")]
+        [Theory]
+        public void ParseMoneyExceptionThrown(string money)
+        {
+            try
+            {
+                ProcessMoney(money);
+            }
+            catch
+            {
+                Assert.True(true);
+            }
+
         }
     }
 }
